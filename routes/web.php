1d5899b8641c186fe2bb1a718fdd224965da8554
Allen Dale Gabisan <allen@usjr.edu.ph>
2017-11-04 01:55:09 +0800

    Initialized routes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,25 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/build-profile', 'ProfileController@build_profile');
+
+Route::get('/me', 'ProfileController@me');
+
+Route::get('/clanalbum', 'PhotoController@clanalbum');
+
+Route::get('/clan-album', 'PhotoController@clan_album');
+
+Route::get('/user_album', 'PhotoController@user_album');
+
+Route::get('/events', 'OhanaController@events');
+
+Route::get('/genealogy','OhanaController@genealogy');
+
+// Route::get('/help', 'OhanaController@help');
+
+// Route::get('/settings', 'OhanaController@settings');
+
+Route::get('/timeline', 'OhanaController@timeline');

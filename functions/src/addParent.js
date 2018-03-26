8f253e796298b07959fdb3dee1179ff461fd5f91
Allen Dale Gabisan <allen@usjr.edu.ph>
2018-03-26 15:40:05 +0800

    Fixed return in promise

--- functions/src/addParent.js
+++ functions/src/addParent.js
@@ -75,8 +75,8 @@ function connectCurrentUserParents(uid, clanId, key, parentType) {
     const parent = userTreeRef.child(`${clanId}/${uid}`).once('value')
 
     if (parentType === 'mother') {
-        parent.then(snapshot => {
-            if (!(snapshot.val().f === null || snapshot.val().f === undefined)) {
+        return parent.then(snapshot => {
+            if (!(snapshot.val().f === undefined || snapshot.val().f === null)) {
                 const pr1 = userTreeRef.child(`${clanId}/${key}/vir`).push(snapshot.val().f).then(() => {
                     return userTreeRef.child(`${clanId}/${key}/ms/${snapshot.val().f}`).set('married')
                 })
@@ -93,8 +93,8 @@ function connectCurrentUserParents(uid, clanId, key, parentType) {
             }
         })
     } else if (parentType === 'father') {
-        parent.then(snapshot => {
-            if (!(snapshot.val().m === null || snapshot.val().m === undefined)) {
+        return parent.then(snapshot => {
+            if (!(snapshot.val().m === undefined || snapshot.val().m === null)) {
                 const pr1 = userTreeRef.child(`${clanId}/${key}/ux`).push(snapshot.val().m).then(() => {
                     return userTreeRef.child(`${clanId}/${key}/ms/${snapshot.val().m}`).set('married')
                 })

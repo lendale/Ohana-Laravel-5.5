62c7e79c60702849ab84860cf92e768327b27448
Allen Dale Gabisan <allen@usjr.edu.ph>
2018-04-26 23:45:09 +0800

    Created a test query function

--- public/assets/js/genealogy.js
+++ public/assets/js/genealogy.js
@@ -100,6 +100,13 @@ function getAvailableParents(uid) {
         `);
     single.appendTo("#parents_container");
 
+    // userTreeRef.child(`${userClanId}/${currentUser.uid}/ux`).once('value').then(snap => {
+    //     if (snap.exists()) {
+    //         motherKeys = Object.keys(snap.val());
+    //         motherNames = Object.values(snap.val());
+    //     }
+    // })
+
     userFamilyRef.child(uid).child('spouse_keys').child('ux').once('value').then(snap => {
 
         if (!(snap.val() === undefined || snap.val() === null)) {
@@ -417,5 +424,34 @@ $(document).ready(function() {
     $('#save_child').click(function() {
         addChild();
         resetForm();
+    })
+
+    // userFamilyRef.child('-LAyiGdITnq2BBb_GwK4').once('value').then(snap => {
+    //     console.log('SNAP', snap.val())
+    // })
+
+    // userFamilyRef.once('value').then(snap => {
+    //     // console.log('SNAP', snap.val())
+    //     snap.forEach(childSnap => {
+    //         let val = childSnap.val()
+
+    //         let mothers = val.mothers
+
+    //         if (!(val.fathers === null || val.fathers === undefined)) {
+    //             let fathers = Object.entries(val.fathers)
+
+
+    //             console.log('FA', fathers)
+    //         }
+
+    //         // console.log('VAL', val.fathers)
+    //     })
+    // })
+
+    userTreeRef.once('value').then(snap => {
+        console.log('SNAP', snap.val())
+            // snap.val().forEach(childSnap => {
+            //     console.log('childSNap', childSnap.val())
+            // })
     })
 })
